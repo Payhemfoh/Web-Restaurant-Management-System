b0fff72a69846e7ef1bf16277f8de575f198353c
NicoleTan0103 <67089285+NicoleTan0103@users.noreply.github.com>
2020-11-12 23:15:57 +0800

    Update orderList.php

--- webpage/orderList.php
+++ webpage/orderList.php
@@ -9,10 +9,76 @@
         ?>
     </head>
 
+
+<style>
+body {
+  font-family: "Lato", sans-serif;
+}
+
+.sidenav {
+  width: 148px;
+  margin-top: 70px;
+  position: fixed;
+  z-index: 1;
+  top: 20px;
+  left: 10px;
+  background: #eee;
+  padding: 8px 0;
+}
+
+.sidenav a {
+  padding: 6px 8px 6px 16px;
+  text-decoration: none;
+  font-size: 22px;
+  color: #2196F3;
+  display: block;
+}
+
+.sidenav a:hover {
+  color: #064579;
+}
+
+
+@media screen and (max-height: 450px) {
+  .sidenav {padding-top: 15px;}
+  .sidenav a {font-size: 18px;}
+}
+</style>
+
     <body class="bg-light">
         <?php printHeader(basename(__FILE__)); ?>
-
         <br/>
+
+        <div class="sidenav">
+
+        <?php
+
+        //database connection
+        $connect = new mysqli("localhost","root","","rms_database");
+
+        //check connection
+        if($connect->connect_error){
+            die("Connection error : $connect->connect_errno : $connect->connect_error");
+        }
+
+        $sql = "SELECT category_name FROM menu_category";
+        $result = $connect->query($sql);
+
+        if ($result->num_rows > 0) 
+        {
+            // output data of each row
+             
+            while($row = $result->fetch_assoc()) 
+            {
+                echo "<b>";
+                echo $row["category_name"];
+                echo "</b>";
+                echo "<br>";
+            }
+        }
+        ?>
+        </div>
+
         <div id="content" class="container bg-light col-md-9 rounded">
             <?php include("../webpage/categoryList.php");?>
         </div>
